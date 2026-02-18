164f5f42fadc6a8f52a73b3df41367c45a3c1e61
somyag910-droid <tumhara-somyag910@gmai.com>
2026-02-18 07:29:50 +0530

    Day 2: Variables and Data Types example

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Variables and Data Types Example
+
+name = "Somya"       # string
+age = 25             # integer
+height = 5.4         # float
+is_student = True    # boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
