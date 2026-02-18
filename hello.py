7736e97f0d8d14f84056057554d337fcd3ac0900
somyag910-droid <tumhara-somyag910@gmai.com>
2026-02-18 07:06:11 +0530

    First Python program

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello, Somya! My first GitHub project.")
+print("Hello, Somya! My first GitHub project.")
+print("Hello, somya! My first GitHub project.")
+print("Hello, Somya! My first Python program.")
+
