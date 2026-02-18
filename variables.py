6c80117dc955f5335d75d80e71c4515bb228cf6a
somyag910-droid <tumhara-somyag910@gmai.com>
2026-02-18 07:36:22 +0530

    Day 2: Practice Variables

--- variables.py
+++ variables.py
@@ -9,3 +9,11 @@ print("Name:", name)
 print("Age:", age)
 print("Height:", height)
 print("Student:", is_student)
+city = "Bangalore" 
+year =2026
+temperature = 28.5
+is_raining =  False
+print("city:", city)
+print("year:", year)    
+print("temperature:", temperature)
+print("is raining:", is_raining)
