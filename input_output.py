4f34605aee962f264c097b5b5aba192e679985a1
somyag910-droid <tumhara-somyag910@gmai.com>
2026-02-18 07:42:28 +0530

    Day 3: Input and Output example

---
+++ input_output.py
@@ -0,0 +1,9 @@
+# Day 3: Input and Output Example
+
+# Taking input from user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Showing output
+print("Hello,", name, "!")
+print("You are", age, "years old.")
